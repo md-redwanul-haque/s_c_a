46bba3ee6d29a16c49d11dc1ce6879a3f809d9a0
md-redwanul-haque <redwan229900@gmail.com>
2025-12-16 12:37:08 +0600

    Initial Commit

---
+++ s_first/s_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  s_firstApp.swift
+//  s_first
+//
+//  Created by Md Redwanul Haque on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct s_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

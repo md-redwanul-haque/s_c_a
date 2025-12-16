c766cc6825515215938e82da94dadec6e08db741
md-redwanul-haque <redwan229900@gmail.com>
2025-12-16 18:44:33 +0600

    day 3 , added custom modifier

--- s_first/day2_c.swift
+++ s_first/day2_c.swift
@@ -11,60 +11,86 @@ import SwiftUI
 
 struct day2_c: View {
     var body: some View {
-        VStack (spacing :0) {
-            HStack() {
-                Text("Hello")
-                Text("World!")
-            }
-            .frame( maxWidth: .infinity,)
-            .frame(height: 40)
-            .background(
-                 RoundedRectangle(cornerRadius: 12)
-                     .fill(Color.red)
-             )
-            .padding(.horizontal)
-       
-            HStack() {
-                Text("Hello")
-                Text("World!")
-            }
-            .frame( maxWidth: .infinity,)
-            .frame(height: 40)
-            .background(
-                 RoundedRectangle(cornerRadius: 12)
-                     .fill(Color.red)
-             )
-            .padding()
-            
-        }
         VStack {
             HStack {
+                Image(systemName: "person.fill")
+                    .CustomIconDesign()
                  Text("Name:")
+                    .titleStyle()
                  Spacer()
                  Text("John")
+                
              }
             .font(.title)
             .padding()
              HStack {
+                 Image(systemName: "mail.fill")
+                     .CustomIconDesign()
                  Text("Email:")
+                     .titleStyle()
                  Spacer()
                  Text("john@test.com")
              }
              .font(.title)
              .padding()
+ 
+            
+            Text("Press here")
+                .customButtonStyle()
         }
-       
-        .background(
-             RoundedRectangle(cornerRadius: 12)
-                 .fill(Color.black)
-         )
-        .foregroundStyle(.red)
-        .frame(maxWidth:.infinity)
-        .padding()
+        .customVStackStyle()
     }
 }
 
 #Preview {
     day2_c()
 }
+
+extension Text {
+    func titleStyle() -> some View {
+        self
+            .font(.title)
+            .bold()
+            .foregroundStyle(.red)
+    }
+}
+
+extension Image {
+    func CustomIconDesign() -> some View {
+        self
+                 .padding()
+                 .frame(width: 45, height: 40)
+                 .background(.gray)
+                 .clipShape(Circle())
+                 .padding(.trailing)
+    }
+}
+
+extension Text {
+    func customButtonStyle() -> some View {
+        self
+            .foregroundStyle(.white)
+            .font(.title)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10,)
+                    .stroke(.white, lineWidth: 15)
+                    .fill(.red)
+            ).padding()
+    }
+}
+
+extension VStack {
+    func customVStackStyle() -> some View {
+        self
+            .background(
+                 RoundedRectangle(cornerRadius: 12)
+                     .fill(Color.black)
+             )
+            .foregroundStyle(.red)
+            .frame(maxWidth:.infinity)
+            .padding()
+    }
+}
+
 

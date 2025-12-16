3e82553c660cb4e190a90dafd82b4e42a342b088
md-redwanul-haque <redwan229900@gmail.com>
2025-12-16 15:52:08 +0600

    for learning

--- s_first/ContentView.swift
+++ s_first/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+            VStack (spacing:20){
+                Text("Top")
+                    .font(.title)
+                    .bold()
+                Text("Middle")
+                    .font(.title)
+                    .bold()
+                Text("Bottom")
+                    .font(.title)
+                    .bold()
+            }
+            .padding(20)
+            .background(Color.black)
+            .foregroundStyle(.red)
+            
+           
+        HStack(alignment:.center) {
+            Text("Big")
+                .font(.largeTitle)
+
+            Text("Small")
         }
-        .padding()
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            Text("On Top")
+                .foregroundStyle(.white)
+        }
+    
     }
 }
 

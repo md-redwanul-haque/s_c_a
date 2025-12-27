cff75f243d76fd816b62e43e9ea7dfc0503e35e4
md-redwanul-haque <redwan229900@gmail.com>
2025-12-27 17:21:49 +0600

    list with identifiable added

---
+++ s_first/day5_list_with_identifiable.swift
@@ -0,0 +1,8 @@
+//
+//  day5_list_with_identifiable.swift
+//  s_practice
+//
+//  Created by Md Redwanul Haque on 27/12/25.
+//
+
+import Foundation
---
+++ s_first/day5_sectionedListView.swift
@@ -0,0 +1,8 @@
+//
+//  day5_sectionedListView.swift
+//  s_practice
+//
+//  Created by Md Redwanul Haque on 27/12/25.
+//
+
+import Foundation
